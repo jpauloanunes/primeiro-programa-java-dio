6340e19d55cce161cebb8f2f4a0585cbd920493c
João Paulo de Arruda <joaopauloarruda85@gmail.com>
2023-03-08 16:29:38 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,7 +4,7 @@ import br.com.dio.model.Pessoa;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		Pessoa pessoa = new Pessoa("Pedro Henrique", "4");
+		Pessoa pessoa = new Pessoa("Pedro Henrique", "5");
 		System.out.println(pessoa);
 	}
 }
